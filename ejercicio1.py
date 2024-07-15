c1b4db0701e7a0d559f23a6f9a63f52fa44fb472
Junnier Anacona <janacona@unicauca.edu.co>
2024-07-15 12:18:27 -0500

    Ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+#Programa para crear un nombre de marca para una cerveza
+print("----------------------------------------------------------------------------------------\n")
+objeto = input("¿Qué objeto te hace pensar inmediatamente en diversión y buenos momentos? ")
+personalidad = input("Si pudieras describir tu personalidad en una palabra, ¿cuál sería? ")
+print("----------------------------------------------------------------------------------------\n")
+
+print("\"The Best\n"+personalidad+objeto+"\"")
+print("----------------------------------------------------------------------------------------")
---
+++ variables_10_7_24.py
@@ -0,0 +1,19 @@
+#Practica con Variables 1
+nombre = "Tony Soprano"
+edad = 51
+print(nombre,edad)
+
+#Practica con Variables 2
+nombre = "Julia"
+apellido = "Roberts"
+nombre_completo = nombre+" "+apellido
+print("Tu nombre completo es: "+nombre_completo)
+
+#Practica con Variables 3
+curso = "Python"
+print("Estás tomando un curso de "+curso)
+
+# git status
+# git add .
+# git commit -m "Ejercicio 1"
+# git push
