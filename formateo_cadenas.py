81cc57e299a1b55cf0bc8ea5b1fc9e5ef3400f31
Junnier Anacona <janacona@unicauca.edu.co>
2024-07-15 19:59:07 -0500

    Ejercicios sobre formateo de Cadenas Literales y practica 2

---
+++ formateo_cadenas.py
@@ -0,0 +1,15 @@
+x = 10
+y= 5
+
+#Cadenas literales
+print("Mis números son {} y {}".format(x,y))
+print("La suma de {} y {} son: {}".format(x,y,x+y))
+
+color = "rojo"
+matricula = 75132
+
+print(f"El auto es {color} y su matricula es {matricula}")
+
+#Casting
+# edad = int(input("Dime tu edad: "))
+# print(type(edad))
